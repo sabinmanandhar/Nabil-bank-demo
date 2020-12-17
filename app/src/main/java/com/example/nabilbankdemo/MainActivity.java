2f7a497abec9d686b15b4eae4e960c83ac21af5c
Sabin Manandhar <sabin.mdhr@gmail.com>
2020-12-17 14:15:57 +0545

    sample demo of nabil bank

---
+++ app/src/main/java/com/example/nabilbankdemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.nabilbankdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
